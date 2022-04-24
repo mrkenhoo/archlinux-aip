d37e93cd13fe9a1d8820b33ba715754070119163
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-04-24 17:29:05 +0100

    Improve code
    - Check if the 'disk' variable does not contains '/dev/' and exit.
    - Clean up some code
    - Add Installer.InstallBootloader()

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace archlinux_aip
 
             if (!PrivilegesManager.IsUserAdmin(username: Environment.UserName))
             {
-                Console.WriteLine("Please execute this program as root");
+                Console.WriteLine("\nPlease execute this program as root");
                 Environment.Exit(1);
             }
 
@@ -21,35 +21,28 @@ namespace archlinux_aip
             Console.WriteLine("\n:: Type the disk you want to format below (e.g.: /dev/sda)");
             string disk = Console.ReadLine();
 
+            if (disk.Contains("/dev/"))
+            {
+                Console.WriteLine($":: ERROR: {disk} Invalid device, try again.");
+                Environment.Exit(1);
+            }
+
             Console.WriteLine("\n:: Specify the size for the boot partition below (default: 500M)");
             string bootSize = Console.ReadLine();
 
             Console.WriteLine("\n:: Specify the size for the root partition below (default: 100G)");
             string rootSize = Console.ReadLine();
 
-            Console.Clear();
-            Console.WriteLine("Welcome to the Arch Linux Automated Installation Tool!");
-            Console.WriteLine("Created by Ken Hoo (mrkenhoo)\n");
-
             DiskManager.FormatDisk(disk: $"{disk}", bootSize: $"{bootSize}", rootSize: $"{rootSize}");
 
             Console.WriteLine("\n:: Specify the filesystem to use below (default: ext4)");
             string filesystem = Console.ReadLine();
 
-            Console.Clear();
-            Console.WriteLine("Welcome to the Arch Linux Automated Installation Tool!");
-            Console.WriteLine("Created by Ken Hoo (mrkenhoo)");
             DiskManager.InstallFilesystem(filesystem: $"{filesystem}", disk: $"{disk}");
-
-            Console.Clear();
-            Console.WriteLine("Welcome to the Arch Linux Automated Installation Tool!");
-            Console.WriteLine("Created by Ken Hoo (mrkenhoo)");
-
-            Console.WriteLine("\nIf you want to override the packages to be installed, type them below");
-            var custom_packages = Console.ReadLine();
 
             Installer.PrepareInstallation(disk: $"{disk}");
             Installer.InstallBaseSystem();
+            Installer.InstallBootloader(disk: $"{disk}");
         }
     }
 }
